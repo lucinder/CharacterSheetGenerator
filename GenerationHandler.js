f42cabd03cea2501c7a3dc0d9c5ee22e43fb0f6c
Sav Wheeler <81818595+lucinder@users.noreply.github.com>
2022-02-16 18:46:17 -0800

    race randomization added

--- GenerationHandler.js
+++ GenerationHandler.js
@@ -1,4 +1,8 @@
+const raceOptions = new Array("Aarakocra","Aasimar","Bugbear","Centaur","Changeling","Dhampir","Dragonborn","Dwarf","Elf","Fairy","Firbolg","Genasi","Gith","Gnome","Goblin","Goliath","Half-Elf","Half-Orc","Halfling","Harengon","Hexblood","Hobgoblin","Human","Kalashtar","Kenku","Kobold","Leonin","Lizardfolk","Loxodon","Merfolk","Minotaur","Orc","Owlin","Reborn","Satyr","Shifter","Simic Hybrid","Tabaxi","Tiefling","Tortle","Triton","Vedalken","Warforged","Yuan-Ti");
+const classOptions = new Array("Artificer");
+
 const FEATURE_ARTIFICER_1_0 = "<p><strong><i>Magical Tinkering.</i></strong></p><p>At 1st level, you've learned how to invest a spark of magic into mundane objects. To use this ability, you must have thieves' tools or artisan's tools in hand. You then touch a Tiny nonmagical object as an action and give it one of the following magical properties of your choice:</p><ul><li>The object sheds bright light in a 5-foot radius and dim light for an additional 5 feet.</li><li>Whenever tapped by a creature, the object emits a recorded message that can be heard up to 10 feet away. You utter the message when you bestow this property on the object, and the recording can be no more than 6 seconds long.</li><li>The object continuously emits your choice of an odor or a nonverbal sound (wind, waves, chirping, or the like). The chosen phenomenon is perceivable up to 10 feet away.</li><li>A static visual effect appears on one of the object's surfaces. This effect can be a picture, up to 25 words of text, lines and shapes, or a mixture of these elements, as you like.</li></ul><p>The chosen property lasts indefinitely. As an action, you can touch the object and end the property early.</p><p> You can bestow magic on multiple objects, touching one object each time you use this feature, though a single object can only bear one property at a time. The maximum number of objects you can affect with this feature at one time is equal to your Intelligence modifier (minimum of one object). If you try to exceed your maximum, the oldest property immediately ends, and then the new property applies.</p>";
+
 
 let ClassFeature = class{
   constructor(lvl, innerHtml){
@@ -28,5 +32,9 @@ const clerFeatures = new Array(new ClassFeature());
 const cleric = DNDClass("Cleric",clerFeatures,8);
 
 function generate(){
+  let level = (int)(Math.random()*20 + 1);
+  let race = raceOptions[(int)(Math.random()*raceOptions.length)];
+  let clss = classOptions[(int)(Math.random()*classOptions.length)];
+  document.getElementById("SHEET_BASIC_RACECLASS").innerHTML = race + " " + clss + " " + level;
  document.getElementById("SHEET_FEATURES_LV1_01").innerHTML = FEATURE_ARTIFICER_1_0;
 }
