678b4af0738a58a7011700bc7098236179812511
Sav Wheeler <81818595+lucinder@users.noreply.github.com>
2022-02-17 13:59:39 -0800

    turning off debug for now

--- GenerationHandler.js
+++ GenerationHandler.js
@@ -1,4 +1,4 @@
-const debug = true; // global debug boolean
+const debug = false; // global debug boolean
 
 const statModifiers = new Array(-5,-4,-4,-3,-3,-2,-2,-1,-1,0,0,1,1,2,2,3,3,4,4,5);
 const pBonuses = new Array(2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6);
