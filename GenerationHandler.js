01076fdedbd349f0d1feee5ae8bdec0e2243b7b3
Sav Wheeler <81818595+lucinder@users.noreply.github.com>
2022-02-16 19:56:02 -0800

    fix level setter

--- GenerationHandler.js
+++ GenerationHandler.js
@@ -11,7 +11,7 @@ let ClassFeature = class{
     this.innerHtml = innerHtml;
   }
   get lvl(){ return this.level; }
-  set lvl(lvl){ this.lvl = lvl; }
+  set lvl(newlvl){ this.lvl = newlvl; }
   get innerHtml(){ return this.innerHtml; }
 }
 
