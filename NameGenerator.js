9cd7602b28819680097b81b5e13c56f22a82da50
Sav Wheeler <81818595+lucinder@users.noreply.github.com>
2022-02-18 08:14:23 -0800

    more updates

--- NameGenerator.js
+++ NameGenerator.js
@@ -6,13 +6,13 @@ let alphSuffixes = new Array(
   new Array("u","ura","uru","ur","ir","iru","el","al","or","ro","ri","as","en","e","a"), // c
   new Array("ae","ay","il","el","a","aph",""), // d
   new Array("lda","ld","st","sta","th","tha","v","va","d"), // e
-  new Array("or","al","ar","in","ay","ae"), // f
-  new Array("il","al","lin","ol","ul"), // g
+  new Array("or","al","ar","in","ay","e","eri","era","er","ir","ri","ra","ro"), // f
+  new Array("il","al","lin","ol","ul","ela"), // g
   new Array("il","ir","ur","al","in","am","iem","a","an"), // h
-  new Array("sta","sti","st","th","n","m","r","l","sa","na","el","e","a","o",""), // i
+  new Array("sta","sti","st","th","n","m","r","l","sa","na","el","e","a","o","te","g","k",""), // i
   new Array("em","arl","ay","ae"), // j
-  new Array("arn","ar","ey","el","uru","ir","al","a","as","e","ae"), // k
-  new Array("i","o","eo","in","or","is","on","ae","ir","ei","er","ia"), // l
+  new Array("arn","ar","ey","el","uru","ir","al","a","as","e","ai"), // k
+  new Array("i","o","eo","in","or","is","on","ae","ir","ei","er","ia","ite"), // l
   new Array("ir","or","in","ay","er","el","o","ol","a","ar"), // m
   new Array("ar","or","ir","el","es","is","at","er","o","a","e","i","iel"), // n
   new Array("s","l","n","ssa","r","th"), // o
@@ -20,10 +20,10 @@ let alphSuffixes = new Array(
   new Array("ues","uen","uin","uil","uay","uor"), // q
   new Array("en","el","il","un","in","ae","ay","iel","ael","is","o","as","es"), // r
   new Array("tar","ar","or","il","ir","at","ata","ay","ae","o","a","el","an","en"), // s
-  new Array("ir","or","ar","yn","in","il","ae","ay","an","ho","hor","han","ha","he","o"), // t
+  new Array("ir","or","ar","yn","in","il","ae","ay","an","ho","hor","han","ha","he","hri","hro","hre","hal","o"), // t
   new Array("thu","th","ne","thay","thae","r","ra","ri","re"), // u
-  new Array("al","alk","ir","in","en","or","ay","ae"), // v
-  new Array("in","yn","il","es","is","en"), // w
+  new Array("al","alk","ir","in","en","or","ay","ei","an","er","era"), // v
+  new Array("in","yn","il","es","is","en","hi"), // w
   new Array("o","a","es","or","ar","an","er","is"), // x
   new Array("is","in","id","ir","a","il","or","r","ssa"), // y
   new Array("eth","ath","il","or","un","a","o","ag","ar")); // z
