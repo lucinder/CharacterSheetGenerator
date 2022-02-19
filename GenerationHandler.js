cdb0ed85e290d98faa69168aae43a43ea851d714
Sav Wheeler <81818595+lucinder@users.noreply.github.com>
2022-02-19 10:03:16 -0800

    bugfix wheeeeeeeeeeeeee

--- GenerationHandler.js
+++ GenerationHandler.js
@@ -1,5 +1,5 @@
-const debug = true; // global debug boolean
-const debugtxt = "";
+let debug = true; // global debug boolean
+let debugtxt = "";
 
 const statModifiers = new Array(-5,-5,-4,-4,-3,-3,-2,-2,-1,-1,0,0,1,1,2,2,3,3,4,4,5);
 const pBonuses = new Array(0,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6);
